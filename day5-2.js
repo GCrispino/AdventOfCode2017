4b87c3e06fb8e53bc4505f551e5ef85e8e75ff15
Crispino <gabriel.crispino@hotmail.com>
2017-12-11 23:00:19 -0300

    Add day 5 second solution

---
+++ day5-2.js
@@ -0,0 +1,23 @@
+
+
+let arr = [1,2,3,4];
+
+const x = arr[0]++,y = arr[0];
+
+const fs = require('fs');
+
+const instructions =
+	fs.readFileSync('inputs/day5input.txt')
+	.toString()
+	.split('\r\n');
+
+let i = 0,nSteps = 0;
+
+for (
+	i = 0, nSteps = 0; 
+	i >= 0 && i < instructions.length;
+	++nSteps
+)
+	i += instructions[i] >= 3 ? instructions[i]-- : instructions[i]++;
+
+console.log('result: ',nSteps);
